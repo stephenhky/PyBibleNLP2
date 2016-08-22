a101969616ac9e70e66a20413ccd32c612a13fb0
stephenhky <stephenhky@yahoo.com.hk>
2016-08-22 17:29:26 -0400

    corpus building code rewritten

--- biblebooks/corpusbuilder.py
+++ biblebooks/corpusbuilder.py
@@ -9,15 +9,34 @@ def build_gensumcorpus(documents):
     corpus = [dictionary.doc2bow(text) for text in documents]
     return dictionary, corpus
 
-def build_biblechapters_corpus(biblesqlite_path, preprocess=lambda text: text):
+def retrieve_docs_as_biblechapters(biblesqlite_path):
+    dbconn = sqlite3.connect(biblesqlite_path)
+    cursor = dbconn.cursor()
+    for book in abbr.otbookdict.keys() + abbr.ntbookdict.keys():
+        for chap in range(1, abbr.numchaps[book]+1):
+            doc_label = book+'_'+chap
+            result = cursor.execute('select scripture from bible where book is "'+book+'" and chapter='+chap)
+            doc_text = reduce(lambda s1, s2: ' '.join(s1, s2), [text for text in result])
+            yield doc_label, doc_text
+    cursor.close()
+
+def retrieve_docs_as_biblebooks(biblesqlite_path):
     dbconn = sqlite3.connect(biblesqlite_path)
     cursor = dbconn.cursor()
     doc_labels = []
     doc_tokens = []
     for book in abbr.otbookdict.keys() + abbr.ntbookdict.keys():
-        for chap in range(1, abbr.numchaps[book]+1):
-            doc_labels += [book+'_'+chap]
-            result = cursor.execute('select scripture from bible where book is "'+book+'" and chapter='+chap)
-            doc_tokens += [reduce(add, [word_tokenize(preprocess(text)) for text in result])]
+        doc_label = book
+        result = cursor.execute('select scripture from bible where book is "'+book)
+        doc_text = reduce(lambda s1, s2: ' '.join(s1, s2), [text for text in result])
+        yield doc_label, doc_text
     cursor.close()
-    return build_gensumcorpus(doc_tokens)
+
+def build_corpus(doc_iterator, preprocess=lambda text: text):
+    doc_labels = []
+    doc_tokens = []
+    for doc_label, doc_text in doc_iterator:
+        doc_labels += [doc_label]
+        doc_tokens += [word_tokenize(preprocess(doc_text))]
+    return doc_labels, build_gensumcorpus(doc_tokens)
+
