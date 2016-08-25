017d6e0140b0ff580933a103fe91422f4c13e3b9
stephenhky <cwin31>
2016-08-24 23:29:18 -0400

    argument parser refined

--- SQLiteToGensumCorpys.py
+++ SQLiteToGensumCorpys.py
@@ -8,7 +8,7 @@ def argument_parser():
     parser = argparse.ArgumentParser(description='Converting SQLite Bible to Gensim Corpus')
     parser.add_argument('sqlite_bible_path', help='path of SQLite bible')
     parser.add_argument('target_path_prefix', help='prefix of gensim corpus and dictionary')
-    parser.add_argument('--book', type=bool, default=False, help='books (not chapters) as documents')
+    parser.add_argument('--book', action='store_true', default=False, help='books (not chapters) as documents')
     return parser
 
 if __name__ == '__main__':
@@ -19,9 +19,9 @@ if __name__ == '__main__':
     sqlite_bible = cpbuilder.get_sqlite3_dbconn(args.sqlite_bible_path)
     doc_iterator = cpbuilder.retrieve_docs_as_biblebooks(sqlite_bible) if args.book else cpbuilder.retrieve_docs_as_biblechapters(sqlite_bible)
     print 'Build the corpus'
-    doc_label, dictionary, gensim_corpus = cpbuilder.build_corpus(doc_iterator,
-                                                                  preprocess=lambda s: word_tokenize(prep.preprocess_text(s, pipeline=prep.pipeline1))
-                                                                  )
+    doc_label, (dictionary, gensim_corpus) = cpbuilder.build_corpus(doc_iterator,
+                                                                    preprocess=lambda s: word_tokenize(prep.preprocess_text(s, prep.pipeline1))
+                                                                   )
     print 'Save the corpus'
     io.save_corpus(dictionary, gensim_corpus, args.target_path_prefix)
     io.save_doclabel(doc_label, args.target_path_prefix+'_doclabels.txt')
